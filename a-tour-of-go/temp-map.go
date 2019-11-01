64f6b711dca40c7b0754a170972767982208e5d3
王常林 <wangchanglin@kuaigeng.com>
2019-11-01 16:32:27 +0800

    add new example

---
+++ a-tour-of-go/temp-map.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+/*
+func main() {
+	sm := make([]map[int]string, 5)
+	for i := range sm {
+		sm[i] = make(map[int]string, 1)
+		sm[i][1] = "OK"
+		fmt.Println(sm[i])
+	}
+	fmt.Println(sm)
+}
+*/
+/*
+func main() {
+	m := map[int]string{1: "a", 2: "b", 3: "c", 4: "d", 5: "e"}
+	s := make([]int, len(m))
+	i := 0
+	for k, _ := range m {
+		s[i] = k
+		i++
+	}
+	sort.Ints(s)
+	fmt.Println(s)
+}
+*/
+
