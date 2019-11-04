475572df0a90e3a3e4f7e382f46fa7afc74e995b
王常林 <wangchanglin@kuaigeng.com>
2019-11-04 15:53:13 +0800

    add new

--- a-tour-of-go/temp-map.go
+++ a-tour-of-go/temp-map.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-)
-
 /*
 func main() {
 	sm := make([]map[int]string, 5)
@@ -29,4 +24,44 @@ func main() {
 	fmt.Println(s)
 }
 */
+/*
+func main() {
+	m1 := map[int]string{1: "a", 2: "b", 3: "c", 4: "d", 5: "e"}
+	fmt.Println(m1)
+	m2 := make(map[string]int)
 
+	for k, v := range m1 {
+		m2[v] = k
+	}
+	fmt.Println(m2)
+}
+
+*/
+
+/*
+
+func main() {
+	A(1, 2, 3, 4, 5, 6, 7, 8)
+}
+
+func A(a ...int) {
+	fmt.Print(a)
+}
+
+*/
+
+// 闭包
+/*
+func main() {
+	f := closure(10)
+	fmt.Println(f(1))
+	fmt.Println(f(2))
+}
+
+
+func closure(x int) func(int) int {
+	return func(y int) int {
+		return x + y
+	}
+}
+*/
